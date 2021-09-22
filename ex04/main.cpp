261f656cd238448e17fad57a8f4396af86459d74
ysoroko <ysoroko@student.s19.be>
2021-09-22 14:38:57 +0200

    ex04 finished

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 12:08:02 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/09/21 12:16:51 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/09/22 14:27:22 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,53 @@
 #include <iomanip>
 #include <fstream>
 
+void	ft_replace(std::string *line, std::string to_r, std::string r_w)
+{
+	size_t	start_pos;
+
+	while ((start_pos = (*line).find(to_r)) != std::string::npos)
+	{
+		(*line).erase(start_pos, to_r.length());
+		(*line).insert(start_pos, r_w);
+	}
+}
+
 int	main(int argc, char **argv)
 {
-	if (argc != 4)
+	if (argc != 4 || !argv[2][0] || !argv[3][0])
 	{
-		std::cout << "Please provide file_name str_to_replace str_substitute as arguments" << std::endl;
+		std::cout << "Program arguments error" << std::endl;
 		return (-1);
 	}
 	// input filestream
+	// alternative:
+	// fstream file;
+	// file.open("test.txt", ios :: in);
 	std::ifstream	ifs(argv[1]);
+	if (!ifs.is_open())
+	{
+		std::cout << "Coulnd't open the input file" << std::endl;
+		return (-1);
+	}
+	std::string	file_name = (std::string)argv[1] + ".replace";
 	// output filestream
-	std::ofstream	ofs(argv[1] + ".replace");
-
+	// alternative:
+	// fstream file;
+	// file.open("test.txt", ios :: out);
+	std::ofstream	ofs(file_name);
+	if (!ofs.is_open())
+	{
+		std::cout << "Coulnd't open the output file" << std::endl;
+		return (-1);
+	}
+	std::string	line_read;
+	std::string	to_replace = argv[2];
+	std::string	replace_with = argv[3];
+	while (getline(ifs, line_read))
+	{
+		ft_replace(&line_read, to_replace, replace_with);
+		ofs << line_read << std::endl;
+	}
 	ofs.close();
 	ifs.close();
 }
